1abc4db6edff1b81675f537d6dacd44aad7b372e
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-06-26 01:29:40 +0600

    Folder permission updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            PermissionSeeder::class,
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
